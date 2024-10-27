e38581ab6f1e1aa27bb52968e174bd54860e426a
Bernard Moy <bernardmoy3@gmail.com>
2024-10-27 15:10:18 +0000

    First unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
+    testImplementation (libs.mockito.core)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
