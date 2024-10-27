4b860bd58fa4316605046f57be67d3e59fa1aeb0
Bernard Moy <bernardmoy3@gmail.com>
2024-10-27 18:57:59 +0000

    User auth

--- app/src/main/java/com/example/lostandfound/ui/login/LoginViewModel.java
+++ app/src/main/java/com/example/lostandfound/ui/login/LoginViewModel.java
@@ -1,6 +1,19 @@
 package com.example.lostandfound.ui.login;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
+
+    // store the textview used to display error
+    private final MutableLiveData<String> loginError = new MutableLiveData<>();
+
+    public MutableLiveData<String> getLoginError(){
+        return this.loginError;
+    }
+
+    public void setLoginError(String s){
+        this.loginError.setValue(s);
+    }
+
 }
