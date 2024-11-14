b63fe754c423d3d0ab3cc2f1dcb9f5eab79a2df4
Bernard Moy <bernardmoy3@gmail.com>
2024-11-14 22:10:36 +0000

    Centering layout

---
+++ app/src/main/java/com/example/lostandfound/ui/ForgotPassword/ForgotPassword.java
@@ -0,0 +1,42 @@
+package com.example.lostandfound.ui.ForgotPassword;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.lostandfound.R;
+import com.example.lostandfound.databinding.ActivityConfirmEmailBinding;
+import com.example.lostandfound.databinding.ActivityForgotPasswordBinding;
+import com.example.lostandfound.ui.ConfirmEmail.ConfirmEmailViewModel;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private ActivityForgotPasswordBinding binding;
+    private ForgotPasswordViewModel forgotPasswordViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        // set up confirm email view model
+        forgotPasswordViewModel = new ViewModelProvider(this).get(ForgotPasswordViewModel.class);
+
+        // inflate binding
+        binding = ActivityForgotPasswordBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
