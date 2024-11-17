a987365f52044d41de6ca9d1a56e8b103a8e7ff9
Bernard Moy <bernardmoy3@gmail.com>
2024-11-17 20:37:20 +0000

    Login checks remake

---
+++ app/src/test/java/com/example/lostandfound/LoginViewModelTest.java
@@ -0,0 +1,83 @@
+package com.example.lostandfound;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import com.example.lostandfound.ui.login.LoginViewModel;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.logging.Handler;
+
+public class LoginViewModelTest {
+
+    // these are valid inputs
+    private String validEmail = "u0000000@warwick.ac.uk";
+    private String validPassword = "yqwe902G*";
+
+    private LoginViewModel viewModel;
+
+    // mock the looper to handle live data
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+    Handler handler = Mockito.mock(Handler.class);
+
+    @Before
+    public void setUp(){
+        viewModel = new LoginViewModel();
+    }
+
+    @Test
+    public void testValidateEmail(){
+        // test for invalid inputs
+        assertFalse(viewModel.validateEmail(""));
+        assertEquals("Email cannot be empty", viewModel.getLoginError().getValue());
+
+        assertFalse(viewModel.validateEmail("eeeeeeewarwick.ac.uk"));
+        assertEquals("Please register with your university email (@warwick.ac.uk)",
+                viewModel.getLoginError().getValue());
+
+        assertFalse(viewModel.validateEmail("eeeeeee@gmail.com"));
+        assertEquals("Please register with your university email (@warwick.ac.uk)",
+                viewModel.getLoginError().getValue());
+
+
+        // test for the valid input
+        assertTrue(viewModel.validateEmail(validEmail));
+    }
+
+    @Test
+    public void testValidatePassword(){
+        // test for invalid inputs
+        assertFalse(viewModel.validatePassword(""));
+        assertEquals("Password cannot be empty", viewModel.getLoginError().getValue());
+
+        assertFalse(viewModel.validatePassword("5tY*)io"));
+        assertEquals("Password must be at least 8 characters long",
+                viewModel.getLoginError().getValue());
+
+        assertFalse(viewModel.validatePassword("weiwewiiw=9*"));
+        assertEquals("Password must have at least one uppercase and lowercase character",
+                viewModel.getLoginError().getValue());
+
+        assertFalse(viewModel.validatePassword("weiweweuUE)"));
+        assertEquals("Password must have at least one numerical character",
+                viewModel.getLoginError().getValue());
+
+        assertFalse(viewModel.validatePassword("weiweEiw5"));
+        assertEquals("Password must have at least one special character",
+                viewModel.getLoginError().getValue());
+
+
+
+        // test for the valid input
+        assertTrue(viewModel.validatePassword(validPassword));
+    }
+
+}
