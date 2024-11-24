a634b96c4b3eb8cf882e719363efed2bcd69de71
Bernard Moy <bernardmoy3@gmail.com>
2024-11-24 15:38:44 +0000

    Large buttons on main activity

--- app/src/main/java/com/example/lostandfound/ui/Home/HomeFragment.java
+++ app/src/main/java/com/example/lostandfound/ui/Home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.lostandfound.ui.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.lostandfound.databinding.FragmentHomeBinding;
+import com.example.lostandfound.ui.NewFound.NewFoundActivity;
+import com.example.lostandfound.ui.NewLost.NewLostActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -22,6 +25,24 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        // buttons for i have lost and i have found
+        binding.lostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), NewLostActivity.class);
+                startActivity(i);
+            }
+        });
+
+        binding.foundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), NewFoundActivity.class);
+                startActivity(i);
+            }
+        });
 
         return root;
     }
