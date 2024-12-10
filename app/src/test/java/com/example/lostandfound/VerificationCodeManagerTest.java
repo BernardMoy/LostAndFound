54789009f10a63d2e68a1baafe164ba8d73c60d6
Bernard Moy <bernardmoy3@gmail.com>
2024-12-10 15:26:21 +0000

    Verf manager test for failure

--- app/src/test/java/com/example/lostandfound/VerificationCodeManagerTest.java
+++ app/src/test/java/com/example/lostandfound/VerificationCodeManagerTest.java
@@ -18,6 +18,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
@@ -38,6 +39,7 @@ public class VerificationCodeManagerTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    // test when user havent generate code before
     @Test
     public void testGenerateVerificationCodeSuccess() throws InterruptedException{
         String testEmail = "test@warwick.ac.uk";
@@ -66,6 +68,86 @@ public class VerificationCodeManagerTest {
             public void onCodeGenerated(String error, String code) {
                 assertEquals("", error);
                 assertEquals(6, code.length());
+                latch.countDown();
+            }
+        });
+        latch.await();
+    }
+
+    // test when user generated code very long ago
+    @Test
+    public void testGenerateVerificationCodeSuccess2() throws InterruptedException{
+        String testEmail = "test@warwick.ac.uk";
+        verificationCodeManager = new VerificationCodeManager(testEmail, mockFirestoreManager);
+
+        // simulate when the get method is used on this email, it returns code with timestamp = 0
+        // indicating the user has generated a code very long ago
+        doAnswer(invocation -> {
+            FirestoreManager.Callback<Map<String, Object>> callback = invocation.getArgument(2);
+
+            Map<String, Object> result = new HashMap<>();
+            result.put(FirestoreNames.USER_VERIFICATIONS_HASHEDCODE, "000000");
+            result.put(FirestoreNames.USER_VERIFICATIONS_TIMESTAMP, 0L);
+
+            callback.onComplete(result);
+            return null;
+        }).when(mockFirestoreManager).get(eq(FirestoreNames.COLLECTION_USER_VERIFICATIONS), eq(testEmail), any(FirestoreManager.Callback.class));
+
+        // mock the put method to the database after the code is generated
+        doAnswer(invocation -> {
+            FirestoreManager.Callback<Boolean> callback = invocation.getArgument(3);
+            callback.onComplete(true);      // assume put always return true
+            return null;
+        }).when(mockFirestoreManager).put(eq(FirestoreNames.COLLECTION_USER_VERIFICATIONS), eq(testEmail), any(Map.class), any(FirestoreManager.Callback.class));
+
+        // code generation should be successful, returning no errors
+        final CountDownLatch latch = new CountDownLatch(1);
+        verificationCodeManager.generateNewVerificationCode(new CodeGenerationCallback() {
+            @Override
+            public void onCodeGenerated(String error, String code) {
+                assertEquals("", error);
+                assertEquals(6, code.length());
+                latch.countDown();
+            }
+        });
+        latch.await();
+    }
+
+
+    // test when user generated code recently, an error message should appear
+    @Test
+    public void testGenerateVerificationCodeFail() throws InterruptedException{
+        String testEmail = "test@warwick.ac.uk";
+        verificationCodeManager = new VerificationCodeManager(testEmail, mockFirestoreManager);
+
+        // simulate when the get method is used on this email, it returns code adn timestamp = current time -30s
+        // the user should not be able to generate a new code
+        doAnswer(invocation -> {
+            FirestoreManager.Callback<Map<String, Object>> callback = invocation.getArgument(2);
+
+            Map<String, Object> result = new HashMap<>();
+            result.put(FirestoreNames.USER_VERIFICATIONS_HASHEDCODE, "000000");
+            long testTimeStamp = Calendar.getInstance().getTimeInMillis() - 30*1000;
+            result.put(FirestoreNames.USER_VERIFICATIONS_TIMESTAMP, testTimeStamp);
+
+            callback.onComplete(result);
+            return null;
+        }).when(mockFirestoreManager).get(eq(FirestoreNames.COLLECTION_USER_VERIFICATIONS), eq(testEmail), any(FirestoreManager.Callback.class));
+
+        // mock the put method to the database after the code is generated
+        doAnswer(invocation -> {
+            FirestoreManager.Callback<Boolean> callback = invocation.getArgument(3);
+            callback.onComplete(true);      // assume put always return true
+            return null;
+        }).when(mockFirestoreManager).put(eq(FirestoreNames.COLLECTION_USER_VERIFICATIONS), eq(testEmail), any(Map.class), any(FirestoreManager.Callback.class));
+
+        // code generation should be successful, returning no errors
+        final CountDownLatch latch = new CountDownLatch(1);
+        verificationCodeManager.generateNewVerificationCode(new CodeGenerationCallback() {
+            @Override
+            public void onCodeGenerated(String error, String code) {
+                assertEquals("Please wait for 1 minute before generating another code", error);
+                assertEquals("", code);
                 latch.countDown();
             }
         });
