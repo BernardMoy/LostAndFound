23f0e5988b8a74816742361628cdbc129a6dc5d3
Bernard Moy <bernardmoy3@gmail.com>
2024-12-11 00:33:36 +0000

    Integrated the compose EditProfile Activity with java code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,5 +81,6 @@ dependencies {
     implementation ("androidx.compose.material3:material3")
     implementation ("androidx.compose.runtime:runtime-livedata")
     implementation ("androidx.compose.ui:ui-tooling")
+    implementation ("androidx.activity:activity-compose:1.3.0-alpha07")
 
 }
