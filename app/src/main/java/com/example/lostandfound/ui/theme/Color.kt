c716789ea3db2fb32a85aac197db6c23eaac3143
Bernard Moy <bernardmoy3@gmail.com>
2024-12-12 23:29:12 +0000

    Color set up

---
+++ app/src/main/java/com/example/lostandfound/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.example.lostandfound.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors
+val PrimaryColor = Color(0xFF5DAADE)
+val PrimaryColorVariant = Color(0xFF4099BC)
+val PrimaryColorLight = Color(0xFFA0DBE6)
+val OnPrimary = Color(0xFFFFFFFF)
+
+// Secondary colors
+val SecondaryColor = Color(0xFF697BD7)
+val SecondaryColorVariant = Color(0xFF4E68AB)
+val SecondaryColorLight = Color(0xFF8EA9EF)
+val OnSecondary = Color(0xFFFFFFFF)
+
+// Background colors
+val BackgroundColorLight = Color(0xFFF6F6F6)
+val BackgroundColorDark = Color(0xFF1B1B1B)
+val OnBackgroundLight = PrimaryColorVariant
+val OnBackgroundDark = PrimaryColorLight
+
+// Error colors
+val Error = Color(0xFFF5425D)
+val ErrorDark = Color(0xFF7C2633)
+val ErrorLight = Color(0xFFEDB4BD)
+
+// Basic colors
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val DarkGray = Color(0xFF535556)
+val Gray = Color(0xFF908A8A)
+val LightGray = Color(0xFFE7E7E7)
---
+++ app/src/main/java/com/example/lostandfound/ui/theme/Shapes.kt
@@ -0,0 +1,7 @@
+package com.example.lostandfound.ui.theme
+
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes(
+
+)
