5d71ecc1e3a774007ac44acddf65fa757ca30631
Bernard Moy <bernardmoy3@gmail.com>
2024-12-13 01:20:25 +0000

    Reminder message

--- app/src/main/java/com/example/lostandfound/ui/theme/Color.kt
+++ app/src/main/java/com/example/lostandfound/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package com.example.lostandfound.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Basic colors
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val DarkGray = Color(0xFF535556)
+val Gray = Color(0xFF908A8A)
+val LightGray = Color(0xFFE7E7E7)
+
 // Primary colors
 val PrimaryColor = Color(0xFF5DAADE)
 val PrimaryColorVariant = Color(0xFF4099BC)
@@ -17,17 +24,10 @@ val OnSecondary = Color(0xFFFFFFFF)
 // Background colors
 val BackgroundColorLight = Color(0xFFF6F6F6)
 val BackgroundColorDark = Color(0xFF1B1B1B)
-val OnBackgroundLight = PrimaryColorVariant
-val OnBackgroundDark = PrimaryColorLight
+val OnBackgroundLight = Black
+val OnBackgroundDark = White
 
 // Error colors
 val Error = Color(0xFFF5425D)
 val ErrorDark = Color(0xFF7C2633)
 val ErrorLight = Color(0xFFEDB4BD)
-
-// Basic colors
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
-val DarkGray = Color(0xFF535556)
-val Gray = Color(0xFF908A8A)
-val LightGray = Color(0xFFE7E7E7)
