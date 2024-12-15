7099d5ca29b526c1b62b1e87acd23be39160f74b
Bernard Moy <bernardmoy3@gmail.com>
2024-12-15 17:46:11 +0000

    Basic notification activity page

--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -35,7 +35,6 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.lostandfound.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
