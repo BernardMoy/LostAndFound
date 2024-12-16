b90760d0d2e2e43bfd1549def031c2c928be4f56
Bernard Moy <bernardmoy3@gmail.com>
2024-12-16 22:59:05 +0000

    Error message diaplaying in edit profile

--- app/src/main/java/com/example/lostandfound/ui/EditProfile/EditProfileViewModel.kt
+++ app/src/main/java/com/example/lostandfound/ui/EditProfile/EditProfileViewModel.kt
@@ -1,9 +1,32 @@
 package com.example.lostandfound.ui.EditProfile
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class EditProfileViewModel : ViewModel(){
-    private val profileError = MutableLiveData("")
+    // mutable live data for first name and last names
+
+    // mutable live data for displaying profile error
+    val profileError: MutableLiveData<String> = MutableLiveData("")
+
+    fun setError(s: String){
+        profileError.value = s
+    }
+
+    // validate the mutable first names and last names, and return true if validated, false otherwise
+    // also sets the profileError accordingly
+    fun validateNames(firstName: String, lastName: String): Boolean{
+        if (firstName.isEmpty()){
+            setError("First name cannot be empty")
+            return false;
+        }
+        if (lastName.isEmpty()){
+            setError("Last name cannot be empty")
+            return false;
+        }
+        setError("")
+        return true
+    }
 
 }
