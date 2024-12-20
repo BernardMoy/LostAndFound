bb79f314ea26524282144a893430411ef6739c80
Bernard Moy <bernardmoy3@gmail.com>
2024-12-20 17:30:26 +0000

    Begin sprint 4 with new lost activity

--- app/src/main/java/com/example/lostandfound/ui/NewLost/NewLostViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.lostandfound.ui.NewLost;
-
-import androidx.lifecycle.ViewModel;
-
-public class NewLostViewModel extends ViewModel {
-
-}
---
+++ app/src/main/java/com/example/lostandfound/ui/NewLost/NewLostViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.lostandfound.ui.NewLost
+
+import androidx.lifecycle.ViewModel
+
+class NewLostViewModel: ViewModel() {
+}
