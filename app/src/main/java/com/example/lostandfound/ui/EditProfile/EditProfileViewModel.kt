fd59924d9bc11dfd727ebd1c1acff766d9ba4d58
Bernard Moy <bernardmoy3@gmail.com>
2024-12-21 15:38:33 +0000

    Refactored data fields to view model in edit profile

--- app/src/main/java/com/example/lostandfound/ui/EditProfile/EditProfileViewModel.kt
+++ app/src/main/java/com/example/lostandfound/ui/EditProfile/EditProfileViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.lostandfound.ui.EditProfile
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class EditProfileViewModel : ViewModel(){
     // mutable live data for first name and last names
-    val firstName: MutableLiveData<String> = MutableLiveData("")
+    val firstName: MutableState<String> = mutableStateOf("")
+    val lastName: MutableState<String> = mutableStateOf("")
 
     // mutable live data for displaying profile error
     val profileError: MutableLiveData<String> = MutableLiveData("")
@@ -14,24 +17,37 @@ class EditProfileViewModel : ViewModel(){
     fun setFirstName(s: String){
         firstName.value = s
     }
-    
+    fun setLastName(s: String){
+        lastName.value = s
+    }
+
     fun setError(s: String){
         profileError.value = s
     }
 
     // validate the mutable first names and last names, and return true if validated, false otherwise
     // also sets the profileError accordingly
-    fun validateNames(firstName: String, lastName: String): Boolean{
-        if (firstName.isEmpty()){
+    fun validateNames(): Boolean{
+        if (firstName.value.isEmpty()){
             setError("First name cannot be empty")
             return false;
         }
-        if (lastName.isEmpty()){
+        if (lastName.value.isEmpty()){
             setError("Last name cannot be empty")
             return false;
         }
         setError("")
         return true
     }
+
+
+    // functions to change the edit text fields within the edit texts
+    fun onFirstNameChanged(s: String){
+        setFirstName(s)
+    }
+    fun onLastNameChanged(s: String){
+        setLastName(s)
+    }
+
 
 }
