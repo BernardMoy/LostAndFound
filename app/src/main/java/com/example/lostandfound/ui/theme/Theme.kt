6652f21890ab3daae62d08d3a9cf6d4730d4cbcd
Bernard Moy <bernardmoy3@gmail.com>
2024-12-25 16:31:46 +0000

    Added outline col

--- app/src/main/java/com/example/lostandfound/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lostandfound/ui/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -31,6 +32,10 @@ private val DarkColorScheme = darkColorScheme(
     surface = BackgroundColorDark,  // adjust based on light / dark
     onSurface = OnBackgroundDark,
     surfaceVariant = Gray,
+
+    outline = Color.Gray,
+    outlineVariant = LightGray
+
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -53,7 +58,10 @@ private val LightColorScheme = lightColorScheme(
 
     surface = BackgroundColorLight,
     onSurface = OnBackgroundLight,
-    surfaceVariant = Gray
+    surfaceVariant = Gray,
+
+    outline = Color.Gray,
+    outlineVariant = LightGray
 )
 
 // "ComposeTheme" is the theme for colors developed in compose
