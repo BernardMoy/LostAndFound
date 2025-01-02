ef85142ca43bc75be2364979bd53f3f6111a5bad
Bernard Moy <bernardmoy3@gmail.com>
2025-01-02 15:31:26 +0000

    Duplicated for found

--- app/src/main/java/com/example/lostandfound/CustomElements/CustomItemPreviews.kt
+++ app/src/main/java/com/example/lostandfound/CustomElements/CustomItemPreviews.kt
@@ -191,3 +191,150 @@ fun CustomLostItemPreview(
         }
     }
 }
+
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun CustomFoundItemPreview(
+    data: Map<String, Any>,
+    onDeleteButtonClicked: () -> Unit = {},
+    onViewButtonClicked: () -> Unit = {}
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = androidx.compose.ui.graphics.Color.Gray,
+                shape = RoundedCornerShape(
+                    dimensionResource(id = R.dimen.corner_radius_small)
+                )
+            )
+            .padding(dimensionResource(id = R.dimen.title_margin))
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin))
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin_half))
+            ) {
+                Text(
+                    text = "Found Entry",
+                    style = Typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Text(
+                    text = "#" + data[FirebaseNames.LOSTFOUND_ID].toString(),
+                    style = Typography.bodyMedium,
+                    color = Color.Gray
+                )
+            }
+
+
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin_half))
+            ) {
+                // the status of the item can be either 0 1 2 - cast them to int
+                val status = (data[FirebaseNames.LOSTFOUND_STATUS] as Long).toInt()
+
+                Icon(
+                    imageVector = Icons.Filled.Circle,
+                    tint = colorResource(
+                        id = statusColor[status] ?: R.color.status0
+                    ),
+                    contentDescription = "Status of item",
+                    modifier = Modifier.width(dimensionResource(id = R.dimen.content_margin))
+                )
+
+                Text(
+                    text = "Status: " + lostStatusText[status],
+                    style = Typography.bodyMedium,
+                    color = colorResource(
+                        id = statusColor[status] ?: R.color.status0
+                    ),
+                    fontWeight = FontWeight.Bold,
+                )
+
+            }
+
+
+            // the main content of the item goes here
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin))
+            ) {
+                // image of the item is loaded using GlideImage
+                GlideImage(
+                    model = (data[FirebaseNames.LOSTFOUND_IMAGE] as Uri),
+                    contentDescription = "Item image",
+                    modifier = Modifier.weight(1F)
+                )
+
+                // other attributes of the item
+                Column(
+                    modifier = Modifier.weight(2F),
+                    verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin_half))
+                ) {
+                    // name
+                    Text(
+                        text = data[FirebaseNames.LOSTFOUND_ITEMNAME].toString(),
+                        style = Typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Bold,
+                    )
+
+                    // date and time
+                    Text(
+                        text = "Date: "
+                                + DateTimeManager.dateTimeToString(
+                            (data[FirebaseNames.LOSTFOUND_EPOCHDATETIME] as? Long) ?: 0L
+                        ),
+                        style = Typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+
+                    // category
+                    Text(
+                        text = "Category: " + data[FirebaseNames.LOSTFOUND_CATEGORY].toString()
+                                + ", " + data[FirebaseNames.LOSTFOUND_SUBCATEGORY].toString(),
+                        style = Typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+
+            // the two buttons of delete and view
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+
+            ) {
+                CustomButton(
+                    text = "Delete",
+                    type = ButtonType.WARNING,
+                    onClick = {
+                        onDeleteButtonClicked()
+                    },
+                    small = true
+                )
+
+                Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.content_margin)))
+
+                CustomButton(
+                    text = "View",
+                    type = ButtonType.FILLED,
+                    onClick = {
+                        onViewButtonClicked()
+                    },
+                    small = true
+                )
+
+            }
+        }
+    }
+}
