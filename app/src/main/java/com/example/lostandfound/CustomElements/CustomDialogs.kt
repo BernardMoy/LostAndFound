f2c3f67e8fe8038b31aa8a17b44ea32f8b5a82d8
Bernard Moy <bernardmoy3@gmail.com>
2025-01-02 22:44:41 +0000

    Location picking dialog

--- app/src/main/java/com/example/lostandfound/CustomElements/CustomDialogs.kt
+++ app/src/main/java/com/example/lostandfound/CustomElements/CustomDialogs.kt
@@ -1,12 +1,19 @@
 package com.example.lostandfound.CustomElements
 
 import android.graphics.Paint.Align
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DatePicker
@@ -14,6 +21,7 @@ import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TimePicker
@@ -26,14 +34,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.lostandfound.Utility.DateTimeManager
 import com.example.lostandfound.R
 import com.example.lostandfound.ui.theme.ComposeTheme
+import com.example.lostandfound.ui.theme.Typography
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -283,30 +297,117 @@ fun CustomLoginDialog(
 
 @Composable
 fun CustomGoogleMapsDialog(
-    isDialogShown: MutableState<Boolean>
+    isDialogShown: MutableState<Boolean>,
+    isEditable: Boolean, // if false, it is in view mode
+    latitude: Double = 52.37930763817003,     // provide lat and long optionally, other it displays this default location
+    longitude: Double = -1.5614912710215834
 ){
     if (isDialogShown.value){
         Dialog(
             onDismissRequest = {
                 isDialogShown.value = false
             },
+            properties = DialogProperties(usePlatformDefaultWidth = false),  // remove horizontal padding
             content = {
-                // Google maps composable
-                val singapore = LatLng(1.35, 103.87)
-                val singaporeMarkerState = rememberMarkerState(position = singapore)
-                val cameraPositionState = rememberCameraPositionState {
-                    position = CameraPosition.fromLatLngZoom(singapore, 10f)
+                Box(
+                    modifier = Modifier
+                        .padding(
+                            vertical = dimensionResource(id = R.dimen.header_margin),
+                            horizontal = dimensionResource(id = R.dimen.title_margin)
+                        )
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius)))
+                        .background(color = MaterialTheme.colorScheme.background),
+
+                ){
+                    Column(
+                        modifier = Modifier.padding(
+                            vertical = dimensionResource(id = R.dimen.title_margin),
+                            horizontal = dimensionResource(id = R.dimen.content_margin)
+                        ),
+                        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin)),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Text(
+                            text = "Select location",
+                            style = Typography.titleMedium,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                        Text(
+                            text = "Tap on the map to select a location.",
+                            style = Typography.bodyMedium,
+                            color = Color.Gray
+                        )
+
+                        // Google maps composable
+                        val location = LatLng(latitude, longitude)
+                        val singaporeMarkerState = rememberMarkerState(position = location)
+                        val cameraPositionState = rememberCameraPositionState {
+                            position = CameraPosition.fromLatLngZoom(location, 15f)
+                        }
+                        GoogleMap(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            cameraPositionState = cameraPositionState
+                        ) {
+                            Marker(
+                                state = singaporeMarkerState,
+                                title = "Singapore",
+                                snippet = "Marker in Singapore"
+                            )
+                        }
+
+                        // buttons
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ){
+                            CustomButton(
+                                text = "Use device location",
+                                type = ButtonType.OUTLINED,
+                                small = true,
+                                onClick = {
+
+                                }
+                            )
+
+                            CustomButton(
+                                text = "Done",
+                                type = ButtonType.FILLED,
+                                onClick = {
+
+                                    // dismiss dialog
+                                    isDialogShown.value = false
+                                }
+                            )
+                        }
+                    }
+
+                    // close button at top end
+                    IconButton(
+                        onClick = {
+                            // remove the image
+                            isDialogShown.value = false
+                        },
+                        modifier = Modifier
+                            .padding(
+                                dimensionResource(id = R.dimen.content_margin_half)
+                            )
+                            .size(dimensionResource(id = R.dimen.image_button_size))
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colorScheme.primary)
+                            .align(Alignment.TopEnd)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Close,
+                            contentDescription = "Close dialog",
+                            tint = Color.White
+                        )
+                    }
+
                 }
-                GoogleMap(
-                    modifier = Modifier.fillMaxSize(),
-                    cameraPositionState = cameraPositionState
-                ) {
-                    Marker(
-                        state = singaporeMarkerState,
-                        title = "Singapore",
-                        snippet = "Marker in Singapore"
-                    )
-                }
+
             }
         )
     }
