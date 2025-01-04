b4a39a40a050020ccbf7076c7a925086806fad6c
Bernard Moy <bernardmoy3@gmail.com>
2025-01-04 14:22:42 +0000

    Converted log activity to kt

---
+++ app/src/main/java/com/example/lostandfound/ui/ActivityLog/ActivityLogActivity.kt
@@ -0,0 +1,80 @@
+package com.example.lostandfound.ui.ActivityLog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.lostandfound.CustomElements.BackToolbar
+import com.example.lostandfound.R
+import com.example.lostandfound.ui.theme.ComposeTheme
+
+
+class ActivityLogActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ActivityLogScreen(activity = this)
+        }
+    }
+}
+
+// mock activity for previews
+class MockActivity : ComponentActivity()
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    ActivityLogScreen(activity = MockActivity())
+}
+
+@Composable
+fun ActivityLogScreen(activity: ComponentActivity) {
+    ComposeTheme {
+        Surface {
+            Scaffold(
+                // top toolbar
+                topBar = {
+                    BackToolbar(title = "Activity Log", activity = activity)
+                }
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues = innerPadding)
+                        .padding(dimensionResource(id = R.dimen.title_margin))
+                        .verticalScroll(rememberScrollState())   // make screen scrollable
+                ) {
+                    // content goes here
+                    MainContent()
+                }
+            }
+        }
+    }
+}
+
+// content includes avatar, edit fields, reminder message and save button
+// get the view model in the function parameter
+@Composable
+fun MainContent(viewModel: ActivityLogViewModel = viewModel()) {
+    // get the local context
+    val context = LocalContext.current
+
+    // boolean to determine if it is being rendered in preview
+    val inPreview = LocalInspectionMode.current
+
+    // content goes here
+}
--- app/src/main/java/com/example/lostandfound/ui/ActivityLog/ActivityLogViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.lostandfound.ui.ActivityLog;
-
-import androidx.lifecycle.ViewModel;
-
-public class ActivityLogViewModel extends ViewModel {
-
-}
