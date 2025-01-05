f708fffc9e73e01375c7a05a18dcf3b46a211a4f
Bernard Moy <bernardmoy3@gmail.com>
2025-01-05 17:49:19 +0000

    Search activity

--- app/src/main/java/com/example/lostandfound/Data/IntentExtraNames.java
+++ app/src/main/java/com/example/lostandfound/Data/IntentExtraNames.java
@@ -7,6 +7,9 @@ public final class IntentExtraNames {
     public static final String INTENT_LOST_ID = "lost_item_id";
     public static final String INTENT_FOUND_ID = "found_item_id";
 
+    // for passing a list of matching found items
+    public static final String INTENT_FOUND_LIST_ID = "found_items";
+
     // string for passing to the done activity
     public static final String INTENT_DONE_ACTIVITY_TITLE = "title";
 
