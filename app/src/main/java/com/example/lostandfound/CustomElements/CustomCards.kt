75593e108f51b81edc5de0b07b63b6cc1be70697
Bernard Moy <bernardmoy3@gmail.com>
2025-01-12 00:46:44 +0000

    Home page pager design

--- app/src/main/java/com/example/lostandfound/CustomElements/CustomCards.kt
+++ app/src/main/java/com/example/lostandfound/CustomElements/CustomCards.kt
@@ -1,56 +1,113 @@
 package com.example.lostandfound.CustomElements
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lostandfound.R
 import com.example.lostandfound.ui.theme.ComposeTheme
+import com.example.lostandfound.ui.theme.Typography
 
 @Preview(showBackground = true)
 @Composable
 fun CustomCardPreview() {
     ComposeTheme {
-        CustomCard(text = "Step 1 is to do something")
+        CustomCard(
+            title = "Search",
+            content = "Search for existing items",
+            icon = Icons.Outlined.Search
+        )
     }
 }
 
 @Composable
 fun CustomCard(
-    text: String
+    title: String,
+    content: String,
+    icon: ImageVector
 ){
-    ElevatedCard(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 6.dp
-        ),
+    Card(
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
+            containerColor = Color.Transparent
         ),
+
         shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small)),
         modifier = Modifier
             .fillMaxWidth()
-            .border(
-                width = 1.dp,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small))
+            .clip(RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small)))  // add rounded corners
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        MaterialTheme.colorScheme.primary,
+                        MaterialTheme.colorScheme.secondary
+                    )
+                ),
             )
+            .padding(dimensionResource(R.dimen.content_margin))
 
     ){
-        Text(
-            modifier = Modifier.padding(dimensionResource(R.dimen.content_margin)),
-            text = text
-        )
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            // large icon
+            Icon(
+                imageVector = icon,
+                contentDescription = "Icon",
+                tint = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .weight(1f)
+                    .size(dimensionResource(id = R.dimen.image_button_size))
+            )
+
+            Column (
+                modifier = Modifier.weight(4f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_margin))
+            ){
+                // title text
+                Text(
+                    text = title,
+                    style = Typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    textAlign = TextAlign.Center
+                )
+
+                // content text
+                Text(
+                    text = content,
+                    style = Typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
