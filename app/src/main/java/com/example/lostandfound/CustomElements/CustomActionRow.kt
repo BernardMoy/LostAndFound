19c7a0dda5d75dc22c055ece07cb0f8205ced145
Bernard Moy <bernardmoy3@gmail.com>
2025-01-17 19:34:23 +0000

    Redesigned settings page

--- app/src/main/java/com/example/lostandfound/CustomElements/CustomActionRow.kt
+++ app/src/main/java/com/example/lostandfound/CustomElements/CustomActionRow.kt
@@ -1,23 +1,22 @@
 package com.example.lostandfound.CustomElements
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import com.example.lostandfound.R
+import com.example.lostandfound.ui.theme.Typography
 
 @Composable
 fun CustomActionRow(
@@ -27,39 +26,54 @@ fun CustomActionRow(
     rightIcon: ImageVector? = Icons.AutoMirrored.Default.KeyboardArrowRight,   // can be null
     tint: Color = MaterialTheme.colorScheme.onBackground     // tint for both text and the icon
 ) {
-    Surface(
-        shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small)),
+    TextField(
+        value = text,
+        onValueChange = {
+            // value wont change
+        },
+        textStyle = Typography.bodyMedium,
+        leadingIcon = {
+            Icon(
+                imageVector = leftIcon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.outline
+            )
+        },
+        trailingIcon = {
+            if (rightIcon != null) {
+                Icon(
+                    imageVector = rightIcon,
+                    contentDescription = null,
+                    tint = Color.Gray
+                )
+            }
+        },
+
         modifier = Modifier
             .fillMaxWidth()
-            .height(dimensionResource(id = R.dimen.image_button_size))
-            .padding(horizontal = dimensionResource(id = R.dimen.content_margin_half)),
-        onClick = onClick
-    ) {
-        Row (
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Icon(imageVector = leftIcon,
-                contentDescription = "Action icon",
-                tint = tint
-            )
-
-            Text(text = text,
-                color = tint,
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier
-                    .weight(1f)  // fill up all middle space
-                    .padding(
-                        start = dimensionResource(id = R.dimen.content_margin),
-                        top = dimensionResource(id = R.dimen.content_margin_half),
-                        bottom = dimensionResource(id = R.dimen.content_margin_half)
-                    )
-            )
-
-            if (rightIcon != null) {
-                Icon(imageVector = rightIcon,
-                    contentDescription = "Redirect",
-                    tint = Color.Gray)
+            .clickable {
+                onClick()
             }
-        }
-    }
+            .padding(
+                horizontal = 0.dp, vertical = dimensionResource(id = R.dimen.content_margin_half)
+            ),
+
+        enabled = false,
+        isError = false,
+        colors = TextFieldDefaults.colors(
+            // for enabled (Editable) text
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+
+            // for disabled (non-editable) text
+            disabledContainerColor = Color.Transparent,
+            disabledTextColor = tint,
+            disabledLabelColor = Color.Gray,
+
+            // remove the default bottom line
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
 }
