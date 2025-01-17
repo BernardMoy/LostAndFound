aeee93ea68a92648a6c5688ab6d27118dc626931
Bernard Moy <bernardmoy3@gmail.com>
2025-01-17 21:32:16 +0000

    Forgot password compose screen

---
+++ app/src/main/java/com/example/lostandfound/ui/ForgotPassword/ForgotPasswordViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.lostandfound.ui.ForgotPassword
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class ForgotPasswordViewModel: ViewModel() {
+    val email: MutableState<String> = mutableStateOf("")
+    val error: MutableState<String> = mutableStateOf("")
+
+    fun onEmailChanged(s: String){
+        email.value = s
+    }
+}
