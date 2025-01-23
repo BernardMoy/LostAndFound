2b582910a220cd7b00472ef941b71ade75cb5dec
Bernard Moy <bernardmoy3@gmail.com>
2025-01-23 23:35:24 +0000

    Font size defined for large font

---
+++ app/src/main/java/com/example/lostandfound/Utility/FontSizeManager.kt
@@ -0,0 +1,8 @@
+package com.example.lostandfound.Utility
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+object FontSizeManager {
+    val isLargeFontSize: MutableState<Boolean> = mutableStateOf(false)
+}
--- app/src/main/java/com/example/lostandfound/ui/theme/Type.kt
+++ app/src/main/java/com/example/lostandfound/ui/theme/Type.kt
@@ -1,29 +1,38 @@
 package com.example.lostandfound.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.lostandfound.R
+import com.example.lostandfound.Utility.FontSizeManager
 
 // Set of Material typography styles to start with
+val isLargeFont by FontSizeManager.isLargeFontSize
+
 val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = if (isLargeFont) 18.sp else 14.sp  // Smaller than bodyLarge
+    ),
 
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp, // Smaller than bodyLarge
-        lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
+        fontSize = if (isLargeFont) 22.sp else 16.sp, // Smaller than bodyLarge
+        lineHeight = if (isLargeFont) 24.sp else 20.sp,
+        letterSpacing = if (isLargeFont) 0.4.sp else 0.25.sp
     ),
 
     titleMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium, // Medium weight text
-        fontSize = 18.sp, // Smaller than titleLarge but still prominent
-        lineHeight = 24.sp,
+        fontSize = if (isLargeFont) 26.sp else 18.sp, // Smaller than titleLarge but still prominent
+        lineHeight = if (isLargeFont) 28.sp else 24.sp,
         letterSpacing = 0.sp
     ),
 )
