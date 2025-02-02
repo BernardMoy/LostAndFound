457fa6994d277035c933e604255dcd7d6292e1da
Bernard Moy <bernardmoy3@gmail.com>
2025-02-02 19:53:46 +0000

    Relocated dev emails

---
+++ app/src/main/java/com/example/lostandfound/Data/Emails.java
@@ -0,0 +1,26 @@
+package com.example.lostandfound.Data;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Emails {
+    private Emails(){}
+
+    /*
+    A list of emails with special access.
+    When registered with this email, an email wont be sent
+    and also user can bypass the verify email process.
+
+    These users also have access to the developer settings if enabled.
+     */
+    public static final Set<String> DEV_EMAILS = Set.of(
+                "testdevholo@warwick.ac.uk",   // u2256784 is not a dev
+                "testdevholo2@warwick.ac.uk",
+                "testdevholo3@warwick.ac.uk"
+    );
+
+    // whether the dev settings are shown to dev users.
+    // turn it off for demo purposes
+    public static final Boolean IS_DEV_SETTINGS_SHOWN = true;
+}
