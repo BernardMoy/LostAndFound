84576c9c144bbb2ccd96fe161d756a86fbbce29f
BernardMoy <bernardmoy3@gmail.com>
2025-02-11 16:27:55 +0000

    Full demo of the image comparison activity except the actual comparison function

--- app/src/main/java/com/example/lostandfound/ui/ImageComparison/ImageComparisonViewModel.kt
+++ app/src/main/java/com/example/lostandfound/ui/ImageComparison/ImageComparisonViewModel.kt
@@ -2,12 +2,14 @@ package com.example.lostandfound.ui.ImageComparison
 
 import android.net.Uri
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class ImageComparisonViewModel: ViewModel() {
     val image1: MutableState<Uri?> = mutableStateOf(null)
     val image2: MutableState<Uri?> = mutableStateOf(null)
+    val distance: MutableState<Double> = mutableDoubleStateOf(0.0)
 
     fun onImage1Picked(uri: Uri?){
         image1.value = uri
@@ -15,5 +17,9 @@ class ImageComparisonViewModel: ViewModel() {
 
     fun onImage2Picked(uri: Uri?){
         image2.value = uri
+    }
+
+    fun onCompareClicked(){
+        distance.value = 4.5
     }
 }
