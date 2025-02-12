d09a4945c97a741746db4cf0caad8282b7b7d244
BernardMoy <bernardmoy3@gmail.com>
2025-02-12 16:01:35 +0000

    Image classifier predict function

--- app/src/main/java/com/example/lostandfound/TFLiteManager/ImageClassifier.kt
+++ app/src/main/java/com/example/lostandfound/TFLiteManager/ImageClassifier.kt
@@ -1,28 +1,37 @@
 package com.example.lostandfound.TFLiteManager
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.provider.MediaStore
 import com.example.lostandfound.ml.SiameseModel
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.TensorFlowLite
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 /*
 val modelFile = context.assets.open(modelFileName).use { it.readBytes() }
         interpreter = Interpreter(modelFile)
  */
 
-class ImageClassifier (context: Context){
+class ImageClassifier (private val context: Context){
     val model = SiameseModel.newInstance(context)
+    val imageSize = 448
 
-    fun predict_bytebuffer(image1: ByteBuffer, image2: ByteBuffer): Float{
+    fun predict_bitmap(image1: Bitmap, image2: Bitmap): Float{
 
         // Creates inputs for reference.
-        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 448, 448, 3), DataType.FLOAT32)
-        inputFeature0.loadBuffer(image1)
-        val inputFeature1 = TensorBuffer.createFixedSize(intArrayOf(1, 448, 448, 3), DataType.FLOAT32)
-        inputFeature1.loadBuffer(image2)
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, imageSize, imageSize, 3), DataType.FLOAT32)
+        val inputFeature1 = TensorBuffer.createFixedSize(intArrayOf(1, imageSize, imageSize, 3), DataType.FLOAT32)
+
+        // Create byte buffer from bitmap, then load the byte buffers into the input features
+        val byteBuffer1: ByteBuffer = bitmapToByteBuffer(image1)
+        inputFeature0.loadBuffer(byteBuffer1)
+        val byteBuffer2: ByteBuffer = bitmapToByteBuffer(image2)
+        inputFeature1.loadBuffer(byteBuffer2)
 
         // Runs model inference and gets result.
         val outputs = model.process(inputFeature0, inputFeature1)
@@ -33,7 +42,17 @@ class ImageClassifier (context: Context){
     }
 
     fun predict(image1: Uri, image2: Uri): Float{
-        return 0.0f
+        // convert the uris to bitmaps
+        val image1src = ImageDecoder.createSource(context.contentResolver, image1)
+        val image2src = ImageDecoder.createSource(context.contentResolver, image2)
+        val image1bm = ImageDecoder.decodeBitmap(image1src)
+        val image2bm = ImageDecoder.decodeBitmap(image2src)
+
+        // resize the bitmaps to the size required by the model
+        val image1bmResized = Bitmap.createScaledBitmap(image1bm, imageSize, imageSize, false)
+        val image2bmResized = Bitmap.createScaledBitmap(image2bm, imageSize, imageSize, false)
+
+        return predict_bitmap(image1bmResized, image2bmResized)
     }
 
     // method to close the model after finish using it
@@ -41,4 +60,28 @@ class ImageClassifier (context: Context){
         model.close()
     }
 
+
+    // utility method to convert bitmap to bytebuffer
+    fun bitmapToByteBuffer(img: Bitmap): ByteBuffer{
+        val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(4*imageSize*imageSize*3)  // 4 for float, 3 for rgb channels
+        byteBuffer.order(ByteOrder.nativeOrder())
+        val intValues = IntArray(imageSize * imageSize)
+        img.getPixels(intValues, 0, img.width, 0, 0, img.width, img.height)
+
+        var pixel: Int = 0
+        for (i in 0 until imageSize){
+            for (j in 0 until imageSize){
+                val value: Int = intValues[pixel++]
+                // get the rgb values and then normalise them to between 0 and 1
+                val red = (value shr 16) and 0xFF
+                val green = (value shr 8) and 0xFF
+                val blue = value and 0xFF
+                byteBuffer.putFloat(red/255f)
+                byteBuffer.putFloat(green/255f)
+                byteBuffer.putFloat(blue/255f)
+            }
+        }
+
+        return byteBuffer
+    }
 }
