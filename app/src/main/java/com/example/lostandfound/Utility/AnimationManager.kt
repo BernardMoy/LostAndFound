b9b2aa27d23a1aed00cc5176e50db04a313a0ca4
BernardMoy <bernardmoy3@gmail.com>
2025-02-14 23:50:04 +0000

    Animation manager to interact with shared pref

--- app/src/main/java/com/example/lostandfound/Utility/AnimationManager.kt
+++ app/src/main/java/com/example/lostandfound/Utility/AnimationManager.kt
@@ -1,8 +1,36 @@
 package com.example.lostandfound.Utility
 
-import androidx.compose.runtime.mutableIntStateOf
+import android.content.Context
 import androidx.compose.runtime.mutableStateOf
+import com.example.lostandfound.Data.SharedPreferencesNames
 
 object AnimationManager {
     val animationEnabled = mutableStateOf(true)
+
+    fun setAnimationEnabled(
+        enabled: Boolean,
+        context: Context
+    ) {
+        val sp = context.getSharedPreferences(
+            SharedPreferencesNames.NAME_ANIMATION,
+            Context.MODE_PRIVATE
+        )
+        sp.edit().putBoolean(SharedPreferencesNames.ANIMATION_VALUE, enabled).apply()
+        animationEnabled.value = enabled
+    }
+
+    // load animation to be called on create in the main activity
+    fun loadAnimationEnabled(
+        context: Context
+    ) {
+        // first get the settings from sp or true if not exist yet
+        val sp = context.getSharedPreferences(
+            SharedPreferencesNames.NAME_ANIMATION,
+            Context.MODE_PRIVATE
+        )
+        val currentAnimationEnabled = sp.getBoolean(SharedPreferencesNames.ANIMATION_VALUE, true)
+
+        // modify the animation value
+        animationEnabled.value = currentAnimationEnabled
+    }
 }
