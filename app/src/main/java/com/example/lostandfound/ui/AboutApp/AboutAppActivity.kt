b2c6d8802052266a7220a5f8c3233586da98090e
BernardMoy <bernardmoy3@gmail.com>
2025-02-18 23:09:50 +0000

    About app screen, pop up text composable and FAQs

--- app/src/main/java/com/example/lostandfound/ui/AboutApp/AboutAppActivity.kt
+++ app/src/main/java/com/example/lostandfound/ui/AboutApp/AboutAppActivity.kt
@@ -1,21 +1,46 @@
 package com.example.lostandfound.ui.AboutApp
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Logout
+import androidx.compose.material.icons.outlined.Description
+import androidx.compose.material.icons.outlined.PrivacyTip
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lostandfound.CustomElements.BackToolbar
+import com.example.lostandfound.CustomElements.CustomActionRow
+import com.example.lostandfound.CustomElements.CustomEditText
+import com.example.lostandfound.CustomElements.CustomGrayTitle
+import com.example.lostandfound.R
 import com.example.lostandfound.ui.theme.ComposeTheme
+import com.example.lostandfound.ui.theme.Typography
 
 
 class AboutAppActivity : ComponentActivity() {
@@ -51,6 +76,8 @@ fun AboutAppScreen(activity: ComponentActivity) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(paddingValues = innerPadding)
+                        .padding(dimensionResource(R.dimen.title_margin))
+                        .verticalScroll(rememberScrollState())
                 ) {
                     // includes the top tab bar and the main content
                     MainContent()
@@ -70,6 +97,85 @@ fun MainContent(viewModel: AboutAppViewModel = viewModel()) {
     // boolean to determine if it is being rendered in preview
     val inPreview = LocalInspectionMode.current
 
+    // contents
+    Column (
+    ){
+        AppInfo(viewModel = viewModel)
+        AppVersion(viewModel = viewModel)
+        PrivacyPolicy(context = context, viewModel = viewModel)
+        EasterEgg(viewModel = viewModel)
+    }
+
+}
+
+// display the app icon and also app name
+@Composable
+fun AppInfo(viewModel: AboutAppViewModel){
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        /*
+        When changing the app icon, aside from changing that in mimmap,
+        also change the app_icon.png (Used to display here) in the drawables folder
+         */
+        Image(
+            painter = painterResource(id = R.drawable.app_icon),
+            contentDescription = "App icon",
+            modifier = Modifier.size(dimensionResource(R.dimen.profile_image_size_large))
+        )
+    }
+
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "LOST AND FOUND",
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            fontWeight = FontWeight.Bold,
+            style = Typography.titleMedium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun AppVersion(viewModel: AboutAppViewModel){
+    CustomGrayTitle("App version")
+    CustomEditText(
+        fieldLabel = "App version",
+        fieldContent = "1.0",
+        leftIcon = Icons.Outlined.Settings,
+        isEditable = false
+    )
+}
+
+@Composable
+fun PrivacyPolicy(context: Context, viewModel: AboutAppViewModel){
+    CustomActionRow(
+        text = "View privacy policy",
+        onClick = {
+            // to be implemented
+            Toast.makeText(context, "To be implemented", Toast.LENGTH_SHORT).show()
+        },
+        leftIcon = Icons.Outlined.PrivacyTip,
+    )
+}
+
+@Composable
+fun EasterEgg(viewModel: AboutAppViewModel){
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "Wait why are you here",
+            color = Color.Gray,
+            style = Typography.bodyMedium,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 
--- app/src/main/java/com/example/lostandfound/ui/HowItWorks/HowItWorksActivity.kt
+++ app/src/main/java/com/example/lostandfound/ui/HowItWorks/HowItWorksActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lostandfound.CustomElements.BackToolbar
+import com.example.lostandfound.CustomElements.CustomPopupText
+import com.example.lostandfound.R
 import com.example.lostandfound.ui.ReportIssue.HowItWorksViewModel
 import com.example.lostandfound.ui.theme.ComposeTheme
 
@@ -52,6 +57,8 @@ fun HowItWorksScreen(activity: ComponentActivity) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(paddingValues = innerPadding)
+                        .padding(dimensionResource(R.dimen.title_margin))
+                        .verticalScroll(rememberScrollState())
                 ) {
                     // includes the top tab bar and the main content
                     MainContent()
@@ -69,6 +76,34 @@ fun MainContent(viewModel: HowItWorksViewModel = viewModel()) {
     // boolean to determine if it is being rendered in preview
     val inPreview = LocalInspectionMode.current
 
+    // content
+    Column(){
+        FAQs(viewModel = viewModel)
+    }
+
+}
+
+@Composable
+fun FAQs(viewModel: HowItWorksViewModel){
+    CustomPopupText(
+        title = "Can I delete my posted items?",
+        content = "You can delete your lost item as long as you haven't made a claim on an item. You can also delete your found item if it has not been claimed."
+    )
+
+    CustomPopupText(
+        title = "How do I contact a user?",
+        content = "You can contact a user in the 'User information' section when viewing an item. You will also find your chat history in the chat tab at the bottom bar at the home page."
+    )
+
+    CustomPopupText(
+        title = "How many claims can I approve?",
+        content = "Unfortunately you can only approve one claim."
+    )
+
+    CustomPopupText(
+        title = "Can I un-approve a claim or unclaim an item?",
+        content = "We are sorry, the app doesn't support these features yet."
+    )
 }
 
 
