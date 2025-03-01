1b7f6349b9fbb283447b213142dba4201749c6a1
BernardMoy <bernardmoy3@gmail.com>
2025-03-01 12:16:07 +0000

    Test get lost from id and test get found from id

--- app/src/androidTest/java/com/example/lostandfound/ItemManagerTest.kt
+++ app/src/androidTest/java/com/example/lostandfound/ItemManagerTest.kt
@@ -1,6 +1,7 @@
 package com.example.lostandfound
 
 import com.example.lostandfound.Data.FirebaseNames
+import com.example.lostandfound.Data.FoundItem
 import com.example.lostandfound.Data.LostItem
 import com.example.lostandfound.FirebaseManagers.ItemManager
 import com.google.android.gms.maps.model.LatLng
@@ -25,7 +26,10 @@ class ItemManagerTest {
     companion object {
         private var firestore: FirebaseFirestore? = null
         private var storage: FirebaseStorage? = null
+
+        // item ids
         private var lost1ID: String? = null
+        private var found1ID: String? = null
 
         @BeforeClass
         @JvmStatic
@@ -67,11 +71,31 @@ class ItemManagerTest {
             FirebaseNames.LOSTFOUND_TIMEPOSTED to 1739941511L
         )
 
+        val dataFound1 = mutableMapOf<String, Any>(
+            FirebaseNames.LOSTFOUND_ITEMNAME to "test",
+            FirebaseNames.LOSTFOUND_USER to "Rwowo",
+            FirebaseNames.LOSTFOUND_CATEGORY to "testCat",
+            FirebaseNames.LOSTFOUND_SUBCATEGORY to "testSubCat",
+            FirebaseNames.LOSTFOUND_COLOR to mutableListOf("Black", "Red"),
+            FirebaseNames.LOSTFOUND_BRAND to "testBrand",
+            FirebaseNames.LOSTFOUND_EPOCHDATETIME to 1738819980L,
+            FirebaseNames.LOSTFOUND_LOCATION to LatLng(52.381162440739686, -1.5614377315953403),
+            FirebaseNames.LOSTFOUND_DESCRIPTION to "testDesc",
+            FirebaseNames.FOUND_SECURITY_Q to "testSecQ",
+            FirebaseNames.FOUND_SECURITY_Q_ANS to "testSecQAns",
+            FirebaseNames.LOSTFOUND_TIMEPOSTED to 1739941511L
+        )
+
         // Post the items
         val task1 = firestore!!.collection(FirebaseNames.COLLECTION_LOST_ITEMS).add(dataLost1)
         val ref1 = Tasks.await(task1, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
         lost1ID = ref1.id
+
+        val task2 = firestore!!.collection(FirebaseNames.COLLECTION_FOUND_ITEMS).add(dataFound1)
+        val ref2 = Tasks.await(task2, 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
+        found1ID = ref2.id
     }
 
     @Test
@@ -102,6 +126,38 @@ class ItemManagerTest {
         assertEquals(Pair(52.381162440739686, -1.5614377315953403), target?.location)
         assertEquals("testDesc", target?.description)
         assertEquals(false, target?.isTracking)
+        assertEquals(1739941511L, target?.timePosted)
+    }
+
+    @Test
+    fun testGetFoundItemFromID(){
+        var target: FoundItem? = null
+        val latch = CountDownLatch(1)
+
+        ItemManager.getFoundItemFromId(found1ID?:"", object: ItemManager.FoundItemCallback{
+            override fun onComplete(foundItem: FoundItem?) {
+                target = foundItem
+                latch.countDown()
+            }
+        })
+        latch.await(60, TimeUnit.SECONDS)
+
+        // assert the item is not null
+        assertNotNull(target)
+
+        // assert each SPECIFIED attribute is correct
+        // the inferred attributes (status etc.) are tested separately
+        assertEquals("test", target?.itemName)
+        assertEquals("Rwowo", target?.userID)
+        assertEquals("testCat", target?.category)
+        assertEquals("testSubCat", target?.subCategory)
+        assertEquals(mutableListOf("Black", "Red"), target?.color)
+        assertEquals("testBrand", target?.brand)
+        assertEquals(1738819980L, target?.dateTime)
+        assertEquals(Pair(52.381162440739686, -1.5614377315953403), target?.location)
+        assertEquals("testDesc", target?.description)
+        assertEquals("testSecQ", target?.securityQuestion)
+        assertEquals("testSecQAns", target?.securityQuestionAns)
         assertEquals(1739941511L, target?.timePosted)
     }
 
