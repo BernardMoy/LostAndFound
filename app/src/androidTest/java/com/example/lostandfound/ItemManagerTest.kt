44ed066370475f8aab8c070a2271d0eed99d97e6
BernardMoy <bernardmoy3@gmail.com>
2025-03-01 01:56:03 +0000

    Success uploading the lost item to emulator

--- app/src/androidTest/java/com/example/lostandfound/ItemManagerTest.kt
+++ app/src/androidTest/java/com/example/lostandfound/ItemManagerTest.kt
@@ -1,20 +1,29 @@
 package com.example.lostandfound
 
 import com.example.lostandfound.Data.FirebaseNames
+import com.example.lostandfound.Data.LostItem
+import com.example.lostandfound.FirebaseManagers.ItemManager
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.storage.FirebaseStorage
+import org.junit.After
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 class ItemManagerTest {
     // static context, visible to the whole class for variables inside here
     companion object {
         private var firestore: FirebaseFirestore? = null
         private var storage: FirebaseStorage? = null
+        private var lost1ID: String? = null
 
         @BeforeClass
         @JvmStatic
@@ -50,7 +59,7 @@ class ItemManagerTest {
             FirebaseNames.LOSTFOUND_COLOR to mutableListOf("Black", "Red"),
             FirebaseNames.LOSTFOUND_BRAND to "testBrand",
             FirebaseNames.LOSTFOUND_EPOCHDATETIME to 1738819980L,
-            FirebaseNames.LOSTFOUND_LOCATION to Pair(52.381162440739686, -1.5614377315953403),
+            FirebaseNames.LOSTFOUND_LOCATION to LatLng(52.381162440739686, -1.5614377315953403),
             FirebaseNames.LOSTFOUND_DESCRIPTION to "testDesc",
             FirebaseNames.LOST_IS_TRACKING to false,
             FirebaseNames.LOSTFOUND_TIMEPOSTED to 1739941511L
@@ -60,10 +69,66 @@ class ItemManagerTest {
         val task1 = firestore!!.collection(FirebaseNames.COLLECTION_LOST_ITEMS).add(dataLost1)
         val ref1 = Tasks.await(task1, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
+        lost1ID = ref1.id
     }
 
     @Test
     fun testGetLostItemFromID() {
+        var target: LostItem? = null
+        val latch = CountDownLatch(1)
 
+        ItemManager.getLostItemFromId(lost1ID?:"", object: ItemManager.LostItemCallback{
+            override fun onComplete(lostItem: LostItem?) {
+                target = lostItem
+                latch.countDown()
+            }
+        })
+        latch.await(60, TimeUnit.SECONDS)
+
+        // assert the item is not null
+        assert(target != null)
+
+        // assert each attribute is correct
+    }
+
+
+
+    // clear all data in firestore after tests
+    @After
+    @Throws(
+        ExecutionException::class,
+        InterruptedException::class,
+        TimeoutException::class
+    )
+    fun tearDown() {
+        // clear all data
+        deleteCollection(FirebaseNames.COLLECTION_LOST_ITEMS)
+        deleteCollection(FirebaseNames.COLLECTION_CLAIMED_ITEMS)
+        deleteCollection(FirebaseNames.COLLECTION_FOUND_ITEMS)
+        deleteCollection(FirebaseNames.COLLECTION_NOTIFICATIONS)
+        deleteCollection(FirebaseNames.COLLECTION_USERS)
+    }
+
+    // private method to delete all elements inside a collection
+    @Throws(
+        ExecutionException::class,
+        InterruptedException::class,
+        TimeoutException::class
+    )
+    private fun deleteCollection(name: String) {
+        val taskGet = firestore!!.collection(name).get()
+        val docs = Tasks.await(taskGet, 60, TimeUnit.SECONDS)
+
+        // create a list of delete tasks for each doc
+        val deleteTasks: MutableList<Task<Void>> = ArrayList()
+        for (doc in docs) {
+            val deleteTask = firestore!!.collection(name)
+                .document(doc.id)
+                .delete()
+            deleteTasks.add(deleteTask)
+        }
+        // execute all tasks
+        Tasks.await(Tasks.whenAll(deleteTasks), 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
     }
 }
