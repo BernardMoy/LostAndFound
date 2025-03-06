67c9c2ef21e45384aca6389a97e393612b361123
BernardMoy <bernardmoy3@gmail.com>
2025-03-06 21:38:03 +0000

    Notif type 1 and 2 test

--- app/src/androidTest/java/com/example/lostandfound/Integration/NotificationType1and2Test.kt
+++ app/src/androidTest/java/com/example/lostandfound/Integration/NotificationType1and2Test.kt
@@ -19,7 +19,6 @@ import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertTrue
 import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Rule
@@ -34,10 +33,13 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
         private var auth: FirebaseAuth? = getAuth()
 
         private var userLostID: String = "SIWSJI2332"
+        private var userLost2ID: String = "289wio2idw"
         private var userFoundID: String? = null
-        private var lostID: String? = "duwindwmw"
-        private var foundID: String? = "2e9j8qijwqiie"
-        private var claimID: String? = "928je29e"
+        private var lostID: String = "duwindwmw"
+        private var lost2ID: String = "2e892iiww"
+        private var foundID: String = "2e9j8qijwqiie"
+        private var claimID: String = "928je29e"
+        private var claim2ID: String = "3289e99e"
         private var dataClaim: Claim? = null   // these are the intents required
 
     }
@@ -72,20 +74,31 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
         assertNotNull(auth!!.currentUser)
 
 
-        // post an user entry of the found user in firestore. this is because it is required in fetching data
         val dataLostUser1 = mutableMapOf<String, Any>(
             FirebaseNames.USERS_EMAIL to "testEmail",
             FirebaseNames.USERS_AVATAR to "",
             FirebaseNames.USERS_FIRSTNAME to "testFirstName",
             FirebaseNames.USERS_LASTNAME to "testLastName"
         )
-
-        // document the found user id and add it
-        val task0 = firestore!!.collection(FirebaseNames.COLLECTION_USERS)
+        val taskU1 = firestore!!.collection(FirebaseNames.COLLECTION_USERS)
             .document(userLostID)
             .set(dataLostUser1)
-        Tasks.await(task0, 60, TimeUnit.SECONDS)
+        Tasks.await(taskU1, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
+
+
+        val dataLostUser2 = mutableMapOf<String, Any>(
+            FirebaseNames.USERS_EMAIL to "testEmail",
+            FirebaseNames.USERS_AVATAR to "",
+            FirebaseNames.USERS_FIRSTNAME to "testFirstName",
+            FirebaseNames.USERS_LASTNAME to "testLastName"
+        )
+        val taskU2 = firestore!!.collection(FirebaseNames.COLLECTION_USERS)
+            .document(userLost2ID)
+            .set(dataLostUser2)
+        Tasks.await(taskU2, 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
+
 
         val dataFoundUser = mutableMapOf<String, Any>(
             FirebaseNames.USERS_EMAIL to "testEmail2",
@@ -93,8 +106,6 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
             FirebaseNames.USERS_FIRSTNAME to "testFirstName2",
             FirebaseNames.USERS_LASTNAME to "testLastName2"
         )
-
-        // document the found user id and add it
         val task1 = firestore!!.collection(FirebaseNames.COLLECTION_USERS)
             .document(userFoundID.toString())
             .set(dataFoundUser)
@@ -103,9 +114,10 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
 
 
         // add the lost item and found item
+        // the item to have claim approved
         val dataLost1 = mutableMapOf<String, Any>(
             FirebaseNames.LOSTFOUND_ITEMNAME to "TestItem",
-            FirebaseNames.LOSTFOUND_USER to userLostID.toString(),
+            FirebaseNames.LOSTFOUND_USER to userLostID,
             FirebaseNames.LOSTFOUND_CATEGORY to "TestCat",
             FirebaseNames.LOSTFOUND_SUBCATEGORY to "TestSubCat",
             FirebaseNames.LOSTFOUND_COLOR to mutableListOf("Black", "Red"),
@@ -116,13 +128,35 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
             FirebaseNames.LOST_IS_TRACKING to false,
             FirebaseNames.LOSTFOUND_TIMEPOSTED to 1739941511L
         )
-
-        // Post the items
         val task2 = firestore!!.collection(FirebaseNames.COLLECTION_LOST_ITEMS)
             .document(lostID.toString()).set(dataLost1)
         Tasks.await(task2, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
 
+
+        // the item to have claim not approved
+        val dataLost2 = mutableMapOf<String, Any>(
+            FirebaseNames.LOSTFOUND_ITEMNAME to "TestItem2",
+            FirebaseNames.LOSTFOUND_USER to userLost2ID,
+            FirebaseNames.LOSTFOUND_CATEGORY to "TestCat2",
+            FirebaseNames.LOSTFOUND_SUBCATEGORY to "TestSubCat2",
+            FirebaseNames.LOSTFOUND_COLOR to mutableListOf("Black", "Red"),
+            FirebaseNames.LOSTFOUND_BRAND to "TestBrand2",
+            FirebaseNames.LOSTFOUND_EPOCHDATETIME to 1738819980L,
+            FirebaseNames.LOSTFOUND_LOCATION to LatLng(52.381162440739686, -1.5614377315953403),
+            FirebaseNames.LOSTFOUND_DESCRIPTION to "TestDesc2",
+            FirebaseNames.LOST_IS_TRACKING to false,
+            FirebaseNames.LOSTFOUND_TIMEPOSTED to 1739941511L
+        )
+
+        // Post the items
+        val task5 = firestore!!.collection(FirebaseNames.COLLECTION_LOST_ITEMS)
+            .document(lost2ID.toString()).set(dataLost2)
+        Tasks.await(task5, 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
+
+
+        // the found item
         val dataFound1 = mutableMapOf<String, Any>(
             FirebaseNames.LOSTFOUND_ITEMNAME to "TestItem",
             FirebaseNames.LOSTFOUND_USER to userFoundID.toString(),
@@ -136,14 +170,13 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
             FirebaseNames.LOST_IS_TRACKING to false,
             FirebaseNames.LOSTFOUND_TIMEPOSTED to 1739941511L
         )
-
-        // Post the items
         val task3 = firestore!!.collection(FirebaseNames.COLLECTION_FOUND_ITEMS)
             .document(foundID.toString()).set(dataFound1)
         Tasks.await(task3, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
 
         // create claim data and post it
+        // claim of lost item 1 to found item
         val dataClaim1 = mutableMapOf<String, Any>(
             FirebaseNames.CLAIM_TIMESTAMP to 1739941511L,
             FirebaseNames.CLAIM_LOST_ITEM_ID to lostID.toString(),
@@ -151,14 +184,27 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
             FirebaseNames.CLAIM_IS_APPROVED to false,
             FirebaseNames.CLAIM_SECURITY_QUESTION_ANS to ""
         )
-
-        // Post the items
         val task4 = firestore!!.collection(FirebaseNames.COLLECTION_CLAIMED_ITEMS)
             .document(claimID.toString()).set(dataClaim1)
         Tasks.await(task4, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
 
-        // set the claim item
+
+        // claim of lost item 2 to found item
+        val dataClaim2 = mutableMapOf<String, Any>(
+            FirebaseNames.CLAIM_TIMESTAMP to 1739941511L,
+            FirebaseNames.CLAIM_LOST_ITEM_ID to lost2ID.toString(),
+            FirebaseNames.CLAIM_FOUND_ITEM_ID to foundID.toString(),
+            FirebaseNames.CLAIM_IS_APPROVED to false,
+            FirebaseNames.CLAIM_SECURITY_QUESTION_ANS to ""
+        )
+        val task6 = firestore!!.collection(FirebaseNames.COLLECTION_CLAIMED_ITEMS)
+            .document(claim2ID.toString()).set(dataClaim2)
+        Tasks.await(task6, 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
+
+
+        // set the claim item (Activity intent to view claim)
         dataClaim = Claim(
             claimID = claimID.toString(),
             lostItemID = lostID.toString(),
@@ -174,7 +220,7 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
     and generates a type 2 notification to other users
      */
     @Test
-    fun testApproveClaim() {
+    fun testNotificationYourClaimApproved() {
         val intent =
             Intent(
                 ApplicationProvider.getApplicationContext(),
@@ -205,7 +251,7 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
         composeTestRule.onNodeWithText("Approve").performClick()
         Thread.sleep(2000)
 
-        // assert a notification is sent
+        // assert a notification of type 1 is sent to the lost user
         val latch = CountDownLatch(1)
         firestore!!.collection(FirebaseNames.COLLECTION_NOTIFICATIONS)
             .whereEqualTo(FirebaseNames.NOTIFICATION_TYPE, 1)
@@ -227,6 +273,27 @@ class NotificationType1and2Test : FirebaseTestsSetUp() {
 
         latch.await(60, TimeUnit.SECONDS)
 
+        // assert a notification of type 2 is sent to the other lost user
+        val latch2 = CountDownLatch(1)
+        firestore!!.collection(FirebaseNames.COLLECTION_NOTIFICATIONS)
+            .whereEqualTo(FirebaseNames.NOTIFICATION_TYPE, 2)
+            .whereEqualTo(FirebaseNames.NOTIFICATION_USER_ID, userLost2ID)
+            .whereEqualTo(FirebaseNames.NOTIFICATION_CLAIM_ID, claim2ID)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                // assert only one item exists
+                assertEquals(1, querySnapshot.size())
+
+                // countdown
+                latch2.countDown()
+
+            }
+            .addOnFailureListener { e ->
+                fail("Failed during db query")
+                latch.countDown()
+            }
+
+        latch2.await(60, TimeUnit.SECONDS)
     }
 
     @After
