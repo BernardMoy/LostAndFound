e225877b836657cf7f0598f1f0f0dcb0580f8ebc
BernardMoy <bernardmoy3@gmail.com>
2025-03-07 01:34:14 +0000

    Notifications type 0 test

--- app/src/androidTest/java/com/example/lostandfound/Integration/NotificationType0Test.kt
+++ app/src/androidTest/java/com/example/lostandfound/Integration/NotificationType0Test.kt
@@ -12,11 +12,14 @@ import com.example.lostandfound.Data.categories
 import com.example.lostandfound.FirebaseTestsSetUp
 import com.example.lostandfound.ui.NewFound.NewFoundScreen
 import com.example.lostandfound.ui.NewFound.NewFoundViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.util.concurrent.CountDownLatch
@@ -25,11 +28,15 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
 
-class NewFoundActivityTest : FirebaseTestsSetUp() {
+class NotificationType0Test : FirebaseTestsSetUp() {
 
     // set up firestore emulator in static context
     companion object {
         private var firestore: FirebaseFirestore? = getFirestore()
+        private var lost1ID: String = "ewioieiwo"
+        private var lost2ID: String = "iweiwioiew"
+        private var userLost1ID: String = "2e929229"
+        private var userLost2ID: String = "289e2iiu"
 
         // store item details
         private val itemName: String = "TESTITEM"
@@ -38,21 +45,60 @@ class NewFoundActivityTest : FirebaseTestsSetUp() {
         private val color1: String = "Black"
         private val color2: String = "Pink"
         private val brand: String = "TESTBRAND"
-        private val description: String = "TESTDESCRIPTION"  // use current date time
-        private val securityQuestion: String = "SECURITYQQQ"
-        private val securityQuestionAns: String = "SECURITYQQQA"
-
+        private val time: Long = 1738819980L
     }
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        // upload two lost item with identical details to the found item
+        // one with tracking one without
+        val dataLost1 = mutableMapOf<String, Any>(
+            FirebaseNames.LOSTFOUND_ITEMNAME to itemName,
+            FirebaseNames.LOSTFOUND_USER to userLost1ID,
+            FirebaseNames.LOSTFOUND_CATEGORY to categoryName,
+            FirebaseNames.LOSTFOUND_SUBCATEGORY to subCategoryName,
+            FirebaseNames.LOSTFOUND_COLOR to mutableListOf(color1, color2),
+            FirebaseNames.LOSTFOUND_BRAND to brand,
+            FirebaseNames.LOSTFOUND_EPOCHDATETIME to time,
+            FirebaseNames.LOSTFOUND_LOCATION to LatLng(52.381162440739686, -1.5614377315953403),
+            FirebaseNames.LOSTFOUND_DESCRIPTION to "",
+            FirebaseNames.LOST_IS_TRACKING to true,
+            FirebaseNames.LOSTFOUND_TIMEPOSTED to time
+        )
+        val task1 = firestore!!.collection(FirebaseNames.COLLECTION_LOST_ITEMS)
+            .document(lost1ID.toString()).set(dataLost1)
+        Tasks.await(task1, 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
+
+
+        val dataLost2 = mutableMapOf<String, Any>(
+            FirebaseNames.LOSTFOUND_ITEMNAME to itemName,
+            FirebaseNames.LOSTFOUND_USER to userLost1ID,
+            FirebaseNames.LOSTFOUND_CATEGORY to categoryName,
+            FirebaseNames.LOSTFOUND_SUBCATEGORY to subCategoryName,
+            FirebaseNames.LOSTFOUND_COLOR to mutableListOf(color1, color2),
+            FirebaseNames.LOSTFOUND_BRAND to brand,
+            FirebaseNames.LOSTFOUND_EPOCHDATETIME to time,
+            FirebaseNames.LOSTFOUND_LOCATION to LatLng(52.381162440739686, -1.5614377315953403),
+            FirebaseNames.LOSTFOUND_DESCRIPTION to "",
+            FirebaseNames.LOST_IS_TRACKING to false,
+            FirebaseNames.LOSTFOUND_TIMEPOSTED to time
+        )
+        val task2 = firestore!!.collection(FirebaseNames.COLLECTION_LOST_ITEMS)
+            .document(lost2ID.toString()).set(dataLost2)
+        Tasks.await(task2, 60, TimeUnit.SECONDS)
+        Thread.sleep(2000)
+    }
 
     /*
     Test that when a found item is posted, its entry appears in the database.
     Remember to start firebase emulator
      */
     @Test
-    fun testPostNewFoundItemInDB() {
+    fun testNewMatchingItemNotification() {
         val viewModel = NewFoundViewModel()
         composeTestRule.setContent {
             NewFoundScreen(
@@ -80,11 +126,6 @@ class NewFoundActivityTest : FirebaseTestsSetUp() {
         Thread.sleep(3000)
         composeTestRule.onNodeWithText("Select").performClick()  // time
         Thread.sleep(2000)
-        composeTestRule.onNodeWithTag("DescriptionInput")
-            .performTextInput(description)  // description
-        composeTestRule.onNodeWithTag("SecurityQuestionInput").performTextInput(securityQuestion)
-        composeTestRule.onNodeWithTag("SecurityQuestionAnsInput")
-            .performTextInput(securityQuestionAns)
 
         // click the done button
         composeTestRule.onNodeWithText("Done").performScrollTo().performClick()
@@ -92,10 +133,10 @@ class NewFoundActivityTest : FirebaseTestsSetUp() {
         // wait for 5 seconds
         Thread.sleep(5000)
 
-        // assert the entry exists in the database
+        // assert only one type 0 notification is sent, and that its user id is correct
         val latch = CountDownLatch(1)
-        firestore!!.collection(FirebaseNames.COLLECTION_FOUND_ITEMS)
-            .whereEqualTo(FirebaseNames.LOSTFOUND_ITEMNAME, itemName)
+        firestore!!.collection(FirebaseNames.COLLECTION_NOTIFICATIONS)
+            .whereEqualTo(FirebaseNames.NOTIFICATION_TYPE, 0)
             .get()
             .addOnSuccessListener { querySnapshot ->
                 // assert only one item exists
@@ -106,28 +147,7 @@ class NewFoundActivityTest : FirebaseTestsSetUp() {
 
                 // verify the attributes are correct
                 assertNotNull(document)
-                assertEquals(itemName, document[FirebaseNames.LOSTFOUND_ITEMNAME] as String)
-                assertEquals(categoryName, document[FirebaseNames.LOSTFOUND_CATEGORY] as String)
-                assertEquals(
-                    subCategoryName,
-                    document[FirebaseNames.LOSTFOUND_SUBCATEGORY] as String
-                )
-                assertEquals(
-                    setOf(color1, color2),
-                    (document[FirebaseNames.LOSTFOUND_COLOR] as List<String>).toSet()
-                )
-                assert(
-                    // reference date. If current datetime is selected it is def greater
-                    document[FirebaseNames.LOSTFOUND_EPOCHDATETIME] as Long > 1739978940
-                )
-
-                assertEquals(brand, document[FirebaseNames.LOSTFOUND_BRAND] as String)
-                assertEquals(description, document[FirebaseNames.LOSTFOUND_DESCRIPTION] as String)
-                assertEquals(securityQuestion, document[FirebaseNames.FOUND_SECURITY_Q] as String)
-                assertEquals(
-                    securityQuestionAns,
-                    document[FirebaseNames.FOUND_SECURITY_Q_ANS] as String
-                )
+                assertEquals(userLost1ID, document[FirebaseNames.NOTIFICATION_USER_ID])
 
                 // countdown
                 latch.countDown()
@@ -152,6 +172,7 @@ class NewFoundActivityTest : FirebaseTestsSetUp() {
     )
     fun tearDown() {
         // clear all data
+        deleteCollection(FirebaseNames.COLLECTION_LOST_ITEMS)
         deleteCollection(FirebaseNames.COLLECTION_FOUND_ITEMS)
         deleteCollection(FirebaseNames.COLLECTION_ACTIVITY_LOG_ITEMS)
         deleteCollection(FirebaseNames.COLLECTION_NOTIFICATIONS)
