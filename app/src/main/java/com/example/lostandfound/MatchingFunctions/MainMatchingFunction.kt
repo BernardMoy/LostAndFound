896ea91194274e96ed4ad6471caa8556e9aaee72
BernardMoy <bernardmoy3@gmail.com>
2025-04-13 21:03:57 +0100

    Search date range update and item weight update

--- app/src/main/java/com/example/lostandfound/MatchingFunctions/MainMatchingFunction.kt
+++ app/src/main/java/com/example/lostandfound/MatchingFunctions/MainMatchingFunction.kt
@@ -24,13 +24,13 @@ In the worst case only the category and color will contribute to the final score
 
 // each of the score will have a range between 0 to 3, where 1.5 is the midpoint
 // the overall score is the weighted mean of score of each attribute
-val WEIGHT_IMAGE = 1              // Low: 1 is the default here
-val WEIGHT_CATEGORY = 1.5          // Higher: Less likely to make mistakes
-val WEIGHT_COLOR = 1.5 // Higher: Less likely to make mistakes with the intelligent algorithm
-val WEIGHT_BRAND = 1.5 // Medium: The same brand may have different representations (Future work)
-val WEIGHT_LOCATION = 1.5         // Medium: The item might be brought elsewhere
+val WEIGHT_IMAGE = 2              // Low: 1 is the default here
+val WEIGHT_CATEGORY = 3          // Higher: Less likely to make mistakes
+val WEIGHT_COLOR = 3 // Higher: Less likely to make mistakes with the intelligent algorithm
+val WEIGHT_BRAND = 2 // Medium: The same brand may have different representations (Future work)
+val WEIGHT_LOCATION = 2         // Medium: The item might be brought elsewhere
 
-val SCORE_THRESHOLD = 2.25  // items will be considered matching if their score is larger than this threshold
+val SCORE_THRESHOLD = 1.5  // items will be considered matching if their score is larger than this threshold
 
 /*
 Given a lost item and found item, return the map containing an overall score
