01f7cf46d9d969050c858c589b67f5f39ca85495
BernardMoy <bernardmoy3@gmail.com>
2025-04-20 18:52:28 +0100

    Update claim timestamp test

--- app/src/androidTest/java/com/example/lostandfound/Unit/UserManagerTest.kt
+++ app/src/androidTest/java/com/example/lostandfound/Unit/UserManagerTest.kt
@@ -1,18 +1,29 @@
 package com.example.lostandfound.Unit
 
 import com.example.lostandfound.Data.FirebaseNames
+import com.example.lostandfound.FirebaseManagers.UserManager
+import com.example.lostandfound.FirebaseManagers.UserManager.UpdateTimeCallback
 import com.example.lostandfound.FirebaseTestsSetUp
+import com.example.lostandfound.Utility.DateTimeManager
 import com.google.android.gms.tasks.Tasks
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.fail
 import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
+import kotlin.math.abs
 
 class UserManagerTest : FirebaseTestsSetUp() {
     // static context, visible to the whole class for variables inside here
     companion object {
+        private var auth: FirebaseAuth = getAuth()
         private var firestore: FirebaseFirestore? = getFirestore()
 
         // item ids
@@ -22,21 +33,79 @@ class UserManagerTest : FirebaseTestsSetUp() {
 
     @Before
     fun setUp() {
-        // add the necessary data to the firebase database
-        val dataUser = mutableMapOf<String, Any>(
-            FirebaseNames.USERS_AVATAR to "ABCDE",
-            FirebaseNames.USERS_EMAIL to "test@warwick.ac.uk",
-            FirebaseNames.USERS_FIRSTNAME to "fnfn",
-            FirebaseNames.USERS_LASTNAME to "lnln"
-        )
+        // create and sign in test user
+        val email = "test@warwick"
+        val password = "1234ABCde"
+        val firstName = "fn"
+        val lastName = "ln"
 
-        // Post the items
-        val task1 = firestore!!.collection(FirebaseNames.COLLECTION_USERS).add(dataUser)
-        val ref1 = Tasks.await(task1, 60, TimeUnit.SECONDS)
+        val latch: CountDownLatch = CountDownLatch(1)
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                if (user != null) {
+                    userID = user.uid
+                }
+                latch.countDown()
+            } else {
+                fail("Failed while creating a user")
+                latch.countDown()
+            }
+        }
+        latch.await()  // wait for user creation
+
+        // after user creation, the user is logged in
+        // assert current user is not null
+        assertNotNull(auth.currentUser)
+
+        val dataUser1 = mutableMapOf<String, Any>(
+            FirebaseNames.USERS_FIRSTNAME to firstName,
+            FirebaseNames.USERS_LASTNAME to lastName,
+            FirebaseNames.USERS_AVATAR to "",
+            FirebaseNames.USERS_EMAIL to email
+        )
+        val task1 = firestore!!.collection(FirebaseNames.COLLECTION_USERS).document(
+            userID.toString()
+        )
+            .set(dataUser1)
+        Tasks.await(task1, 60, TimeUnit.SECONDS)
         Thread.sleep(2000)
-        userID = ref1.id
     }
 
+    @Test
+    fun testUpdateClaimTimestamp() {
+        val latch = CountDownLatch(1)
+        val currentTime = DateTimeManager.getCurrentEpochTime()
+
+        UserManager.updateClaimTimestamp(object : UpdateTimeCallback {
+            override fun onComplete(success: Boolean) {
+                Assert.assertTrue(success)
+                latch.countDown()
+            }
+        })
+        latch.await(60, TimeUnit.SECONDS)
+
+        // now see if the time is correct
+        val latch2 = CountDownLatch(1)
+        firestore!!.collection(FirebaseNames.COLLECTION_USERS)
+            .document(userID ?: "")
+            .get()
+            .addOnSuccessListener { doc ->
+                assert(
+                    abs((doc[FirebaseNames.USERS_LAST_CLAIMED_TIMESTAMP] as Long) - currentTime) < 100
+                )
+
+                // countdown
+                latch2.countDown()
+
+            }
+            .addOnFailureListener { e ->
+                fail("Failed during db query")
+                latch2.countDown()
+            }
+
+        latch2.await(60, TimeUnit.SECONDS)
+    }
 
     // clear all data in firestore after tests
     @After
